a937e91fd02ea25fb373161904da647dfa245495
Dora Do <dora.thien.do@gmail.com>
2022-09-28 13:34:14 -0400

    petbot

--- main.js
+++ main.js
@@ -1,93 +1,78 @@
 import { Application } from '@splinetool/runtime';
 
-let currentScene = 'HI';
+const botui = new BotUI('my-botui-app');
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
 
 const SCENES = {
     BASE: 'https://prod.spline.design/WntXgIyUEdn49DFP/scene.splinecode',
     HI: 'https://prod.spline.design/tm-q8M4UBVnlzfXs/scene.splinecode',
     HAPPY: 'https://prod.spline.design/ipTf4XEj4hAhzts8/scene.splinecode',
 };
-
-initChatBot();
+const PET_TYPES = [
+    {
+        text: 'Dogs',
+        value: 'dogs'
+    },
+    {
+        text: 'Cats',
+        value: 'cats'
+    },
+    {
+        text: 'Both',
+        value: ''
+    },
+];
 
 window.onload = () => {
-    const canvas = document.getElementById('canvas3d');
-    const app = new Application(canvas);
-    app.load(SCENES[currentScene]);
+    updateScene(SCENES.HI);
+    initChatBot();
 }
 
-
-
-
-// Object.keys(SCENES).map(scene => {
-    // const sceneCanvas = document.createElement('canvas');
-    // document.querySelector('.canvas-wrapper').append(sceneCanvas);
-    // const app = new Application(sceneCanvas);
-    // app.load(SCENES[scene]);
-
-    // document.querySelector(`.buttons #${scene.toUpperCase()}`).addEventListener('click', () => {
-    //     currentScene = scene;
-    //     app.load(SCENES[currentScene]);
-    // });
-// });
+function updateScene(scene) {
+    app.load(scene);
+}
 
 async function initChatBot() {
-    const botui = new BotUI('my-botui-app');
+    await sendBotMessage('Hey! I heard you’re looking for a new friend! What’s your name?');
+    const nameResponse = await sendUserTextAction('Enter your name');
+
+    await sendBotMessage(`Nice to meet you ${nameResponse.value}! Could you tell me where you live?`);
+    const addressResponse = await sendUserTextAction('Enter your city');
+    // const shelterIds = await searchShelters(addressResponse.value);
+    // console.log(shelterIds);
+    updateScene(SCENES.HAPPY);
+
+    await sendBotMessage('What kind of friend are you looking for?');
+    const petTypeRes = sendUserButtonAction(PET_TYPES);
+
+    await sendBotMessage(`So you're looking for ${petTypeRes.value.toLowerCase()}! We have plenty of friends like that.`);
+}
+
+async function sendBotMessage(message) {
     await botui.message.bot({
-        content: 'Hey! I heard you’re looking for a new friend! What’s your name?',
-        // delay: 3000,
-        // loading: true,
+        content: message,
+        delay: 3000,
+        loading: true,
     });
-    const nameResponse = await botui.action.text({
+    // scroll to bottom
+}
+
+async function sendUserTextAction(placeholder) {
+    const response = await botui.action.text({
         action: {
-            placeholder: 'Enter your name'
+            placeholder: placeholder
         },
         delay: 1000,
         loading: true,
     });
-    // await botui.message.bot({
-    //     content: `Nice to meet you ${nameResponse.value}! Could you tell me where you live?`,
-    //     delay: 1500,
-    //     loading: true,
-    // });
-    // const addressResponse = await botui.action.text({
-    //     action: {
-    //         placeholder: 'Enter your city'
-    //     }
-    // });
-    // const shelterIds = await searchShelters(addressResponse.value);
-    // console.log(shelterIds);
+    return response;
+}
 
-    currentScene = SCENES.HAPPY;
-    app.load(SCENES[currentScene]);
-
-    await botui.message.bot({
-        content: `Nice to meet you ${nameResponse.value}! What kind of friend are you looking for?`,
-        delay: 1500,
-        loading: true,
-    });
-
-    const petTypeRes = await botui.action.button({
-        action: [
-            {
-                text: 'Dogs',
-                value: 'dogs'
-            },
-            {
-                text: 'Cats',
-                value: 'cats'
-            },
-            {
-                text: 'Both',
-                value: ''
-            },
-        ]
-    });
-
-    await botui.message.bot({
-        content: `So you're looking for ${petTypeRes.value.toLowerCase()}! We have plenty of friends like that.`,
-        delay: 1500,
-        loading: true,
+async function sendUserButtonAction(buttons) {
+    await botui.action.button({
+        action: buttons
     });
 }
 
